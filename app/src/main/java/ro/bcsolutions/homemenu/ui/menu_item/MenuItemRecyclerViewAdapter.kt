b02cb381035acc8dc080c74c94a708fd371b1748
Dragos Burciu <dragos.burciu@gmail.com>
2020-04-14 23:43:46 +0300

    design

--- app/src/main/java/ro/bcsolutions/homemenu/ui/menu_item/MenuItemRecyclerViewAdapter.kt
+++ app/src/main/java/ro/bcsolutions/homemenu/ui/menu_item/MenuItemRecyclerViewAdapter.kt
@@ -1,9 +1,11 @@
 package ro.bcsolutions.homemenu.ui.menu_item
 
+import android.graphics.Color
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.navigation.Navigation
 import androidx.recyclerview.widget.RecyclerView
 import com.google.android.material.floatingactionbutton.FloatingActionButton
@@ -21,6 +23,11 @@ class MenuItemRecyclerViewAdapter: RecyclerView.Adapter<MenuItemRecyclerViewAdap
         notifyDataSetChanged()
     }
 
+    private val today = Calendar.getInstance()
+    private val today_day = today.get(Calendar.DAY_OF_MONTH)
+    private val today_month = today.get(Calendar.MONTH)
+    private val today_year = today.get(Calendar.YEAR)
+
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
         val view = LayoutInflater.from(parent.context).inflate(R.layout.list_item_edit_home_menu_item, parent, false)
         return ViewHolder(view)
@@ -36,7 +43,11 @@ class MenuItemRecyclerViewAdapter: RecyclerView.Adapter<MenuItemRecyclerViewAdap
         holder.dinner.text = item.dinner
         holder.dateDay.text = item.date.get(Calendar.DAY_OF_MONTH).toString()
         holder.dateMonth.text = SimpleDateFormat("MMM").format(item.date.time)
-        holder.button.setOnClickListener(Navigation.createNavigateOnClickListener(EditMenuFragmentDirections.actionNavEditMenuToMenuItem(item.id)))
+        holder.layout.setOnClickListener(Navigation.createNavigateOnClickListener(EditMenuFragmentDirections.actionNavEditMenuToMenuItem(item.id)))
+        if (item.date.get(Calendar.DAY_OF_MONTH) == today_day && item.date.get(Calendar.MONTH) == today_month && item.date.get(Calendar.YEAR) == today_year) {
+            holder.dateDay.setTextColor(Color.GREEN)
+            holder.dateMonth.setTextColor(Color.GREEN)
+        }
     }
 
     class ViewHolder(itemView: View): RecyclerView.ViewHolder(itemView){
@@ -44,6 +55,6 @@ class MenuItemRecyclerViewAdapter: RecyclerView.Adapter<MenuItemRecyclerViewAdap
         val dateMonth: TextView = itemView.findViewById(R.id.menu_date_month)
         val lunch: TextView = itemView.findViewById(R.id.lunch_menu_item)
         val dinner: TextView = itemView.findViewById(R.id.dinner_menu_item)
-        val button: FloatingActionButton = itemView.findViewById(R.id.button_edit_menu_item)
+        val layout: ConstraintLayout = itemView.findViewById(R.id.list_item_edit_home_menu_item_row)
     }
 }
