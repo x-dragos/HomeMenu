9dc6c8ae5e1dad08e05b76365ff39b26cf708761
Dragos Burciu <dragos.burciu@gmail.com>
2020-04-16 21:55:03 +0300

    Homemenu list

--- app/src/main/java/ro/bcsolutions/homemenu/BindingUtils.kt
+++ app/src/main/java/ro/bcsolutions/homemenu/BindingUtils.kt
@@ -1,9 +1,11 @@
 package ro.bcsolutions.homemenu
 
+import android.graphics.Color
 import android.widget.TextView
 import androidx.databinding.BindingAdapter
 import ro.bcsolutions.homemenu.database.MenuItem
 import java.text.SimpleDateFormat
+import java.util.*
 
 @BindingAdapter("shortFormatDateString")
 fun TextView.setShortFormatDateString(item: MenuItem?) {
@@ -16,5 +18,6 @@ fun TextView.setShortFormatDateString(item: MenuItem?) {
 fun TextView.setWeekdayString(item: MenuItem?) {
     item?.let {
         text = SimpleDateFormat("EEE").format(item.date.time)
+        if (item.date.get(Calendar.DAY_OF_WEEK) in arrayOf(1,7)) setTextColor(Color.RED) else setTextColor(Color.BLACK)
     }
 }
